6a1650357c1d7616f73290f28fd09d92c3ed69c2
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-01 21:18:15 +1000

    Add imageList Object files

---
+++ src/ImageList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default {  
+    image1: "./img/test_bg1.png",
+    image2: "./img/test_bg2.png",
+    image3: "./img/test_bg3.png",
+    image4: "./img/test_bg4.png"
+}

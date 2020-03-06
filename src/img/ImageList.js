be9a2ddd8512a3df941c632656550d509451d01e
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-07 03:15:08 +1000

    Rename the images and add thumbnail images

--- src/img/ImageList.js
+++ src/img/ImageList.js
@@ -1,4 +1,4 @@
-export const bg1 = require('./city-landscape-london-england-united-kingdom-1600x2560.jpg');
-export const bg2 = require('./unnamed.jpg')
-export const bg3 = require('./yi-peng.jpg')
-export const bg4 = require('./test_bg4.png')
+export const bg1 = require('./img1.jpg');
+export const bg2 = require('./img2.jpg');
+export const bg3 = require('./img3.jpg');
+export const bg4 = require('./img3.jpg');

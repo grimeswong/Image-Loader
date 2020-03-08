e21ef6dbabbd6006417910ecacba76204873b293
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-08 21:55:36 +1000

    Refine the GetImageURL function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ImageStore from './ImageStore.js'
 import './App.css'
-import GetURLAddress from './util/GetURLAddress.js'
+import { GetImageURL } from './util/GetImageURL.js'
 
 function App() {
+  GetImageURL();
   return (
-    <div className="App" style={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 6%, rgba(0,212,255,1) 100%)"}}>
+    <div className="App">
       <h1>Image Loader</h1>
       <ImageStore />
-      <GetURLAddress />
     </div>
   );
 }
